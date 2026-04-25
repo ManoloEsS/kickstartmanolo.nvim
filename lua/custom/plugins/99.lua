b516ad3ef2503e33eaeff9a50ff242ca1d811f28
ManoloEsS <manolo.estrada.s@gmail.com>
2026-04-25 16:22:49 -0600

    add fast and deep visual mode for 99

--- lua/custom/plugins/99.lua
+++ lua/custom/plugins/99.lua
@@ -13,14 +13,24 @@ return {
         path = '/tmp/' .. basename .. '.99.debug',
         print_on_error = true,
       },
+      md_files = {
+        "AGENT.md",
+        "SKILL.md",
+      },
     })
 
     vim.keymap.set('n', '<leader>9s', function()
       _99.search()
     end, { desc = '[9]9 Search' })
 
-    vim.keymap.set('v', '<leader>9v', function()
+    vim.keymap.set('v', '<leader>9vd', function()
+      _99.set_model("opencode-go/qwen3.5-plus")
       _99.visual()
-    end, { desc = '[9]9 Visual' })
+    end, { desc = '[9]9 Visual [d]efault (qwen3.5-plus)' })
+
+    vim.keymap.set('v', '<leader>9vf', function()
+      _99.set_model("opencode-go/mimo-v2-pro")
+      _99.visual()
+    end, { desc = '[9]9 Visual [f]ast (mimo-v2-pro)' })
   end,
 }
